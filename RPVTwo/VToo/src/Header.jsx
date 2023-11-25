bd113f114a93ac32a5cd87cfea4c5bb7c745d518
RegP <ReggieP33@gmail.com>
2023-11-24 16:18:24 -0800

    Updating Fightstick Section with pictures, edits

--- RPVTwo/VToo/src/Header.jsx
+++ RPVTwo/VToo/src/Header.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className="bg-blue-500 text-white p-4">
+  
+    <header className="bg-blue-500 text-white p-4 w-full" >
       <div className="container mx-auto">
-        <div className="flex items-center justify-between">
-          <div className="text-2xl font-semibold">Your App Name</div>
+        <div className="flex items-center justify-between ">
+          <div className="text-2xl font-semibold px-5">Reggie Pollock, Electrical & Software Engineer</div>
           <nav>
             <ul className="flex space-x-4">
-              <li><a href="/">Home</a></li>
-              <li><a href="/about me">About me</a></li>
-              <li><a href="/portfolio">Portfolio</a></li>
+            <a href="/"><li className="p-5 rounded-md border-white border-2 hover:border-black">Home</li></a>
+              <li className="p-5 rounded-md border-white border-2 hover:border-black"><a href="/portfolio">Projects</a></li>
+              <li className="p-5 rounded-md border-white border-2 hover:border-black"><a href="/resume">Resume</a></li>
             </ul>
           </nav>
         </div>
       </div>
     </header>
+
   );
 }
 
